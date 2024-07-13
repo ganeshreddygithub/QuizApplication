98296c382ffe305e2c1d20a6635d41d992ccae10
ganes <ganes@GANESHREDDYADIDELA>
2024-07-13 17:58:02 +0530

    first commit

---
+++ quiz-service/quiz-service/src/main/java/com/example/quiz_service/model/QuizDto.java
@@ -0,0 +1,30 @@
+package com.example.quiz_service.model;
+
+
+
+
+public class QuizDto {
+    String categoryName;
+    Integer numQuestions;
+    String title;
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public Integer getNumQuestions() {
+		return numQuestions;
+	}
+	public void setNumQuestions(Integer numQuestions) {
+		this.numQuestions = numQuestions;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+    
+    
+}
---
+++ quiz-service/quiz-service/src/main/java/com/example/quiz_service/model/Response.java
@@ -0,0 +1,32 @@
+package com.example.quiz_service.model;
+
+
+
+
+public class Response {
+    
+	private Integer id;
+    private String response;
+    
+    public Response() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+    
+}
